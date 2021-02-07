74277b8e1768e55581c7860c06e8f0cd84ea1ef6
kj_ <karol.sujeta@outlook.com>
2021-02-07 16:26:40 +0100

    Rozwinięcie 'filter.model'

--- src/app/_models/filter.model.ts
+++ src/app/_models/filter.model.ts
@@ -2,12 +2,16 @@ export class FilterModel {
     name: string;
     ip: string;
     port: string;
+    protocole: string;
     amount: string;
+    interface: string;
 
     parseFromObject(object) {
         this.name = object.name;
         this.ip = object.ip;
         this.port = object.port;
+        this.protocole = object.protocole;
         this.amount = object.amount;
+        this.interface = object.interface;
     }
 }
