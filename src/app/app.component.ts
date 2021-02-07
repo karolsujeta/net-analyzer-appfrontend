eb72797ea9bf2fda95375df769733a3943e3c82d
kj_ <karol.sujeta@outlook.com>
2021-02-07 16:26:10 +0100

    Rozwinięcie funkcjinalności i widoków na stronie głównej

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,10 @@ import { FilterService } from './_services/filter.service';
 })
 export class AppComponent {
   title = 'Network Analyzer';
+  public loading: boolean = false;
   public route: string;
+  public configResults: string[] = [];
+  public nmapResults: string[] = [];
 
   constructor(
     public _filter: FilterService,
@@ -20,8 +23,27 @@ export class AppComponent {
     })
   }
 
+  ngOnInit() {
+    this.subscribeNetworkInfo();
+  }
   /**Przeniesienie do szczegółów wybranego filtru */
   openFilter(route: string) {
     this._router.navigate([route]);
+  }
+
+  /**Zasubskrybowanie podstawowych informacji o parametrach sieciowych */
+  subscribeNetworkInfo() {
+    this.loading = true;
+    this._filter.getNetworkInfo()
+      .subscribe((info: any) => {
+        this.nmapResults = info.Payload.nmapResults.split('\n');
+        this.configResults = info.Payload.configResults.split('\n');
+        this._filter.convertInterfacesList(info.Payload.interfacesResults.split('\n'));
+        this.loading = false;
+        console.log("NmapResults:", this.nmapResults, "interfacesResults:", this._filter.networkInterfaces, "configResults:", this.configResults);
+      }, (err) => {
+        this.loading = false;
+        console.error("Coś poszło nie tak z zasubskrybowaniem informacji o parametrach sieciowych...", err);
+      })
   }
 }
