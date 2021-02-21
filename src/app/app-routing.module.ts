0832af60e6c175250d16e50fb619023fa019878f
kj_ <karol.sujeta@outlook.com>
2021-02-21 13:18:51 +0100

    Dodanie komponentu 'NmapAnalysis' do 'app-routing.module'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { IpAnalysisComponent } from './ip-analysis/ip-analysis.component';
+import { NmapAnalysisComponent } from './nmap-analysis/nmap-analysis.component';
 import { PingerComponent } from './pinger/pinger.component';
 import { TcpAnalysisComponent } from './tcp-analysis/tcp-analysis.component';
 import { TrafficAnalysisComponent } from './traffic-analysis/traffic-analysis.component';
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'traffic', component: TrafficAnalysisComponent },
   { path: 'pinger', component: PingerComponent },
   { path: 'tcp', component: TcpAnalysisComponent },
-  { path: 'ip', component: IpAnalysisComponent }
+  { path: 'ip', component: IpAnalysisComponent },
+  { path: 'nmap', component: NmapAnalysisComponent }
 ];
 
 @NgModule({
